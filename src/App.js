d52c4f10f3100948ce9eab8a1c7533bfcc52e099
izzyimac <isabelleaimacseng@gmail.com>
2020-08-24 13:37:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,95 +6,95 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-// class App extends Component {
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            stop being so slowwww
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+// class TodoApp extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { items: [], text: '' };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
 //   render() {
 //     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             stop being so slowwww
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
+//       <div>
+//         <h3>To Do</h3>
+//         <TodoList items={this.state.items} />
+//         <form onSubmit={this.handleSubmit}>
+//           <label htmlFor="new-todo">
+//             What needs to be done?
+//           </label>
+//           <input
+//             id="new-todo"
+//             onChange={this.handleChange}
+//             value={this.state.text}
+//           />
+//           <button>
+//             Add #{this.state.items.length + 1}
+//           </button>
+//         </form>
 //       </div>
+//     );
+//   }
+
+//   handleChange(e) {
+//     this.setState({ text: e.target.value });
+//   }
+
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     if (this.state.text.length === 0) {
+//       return;
+//     }
+//     const newItem = {
+//       text: this.state.text,
+//       id: Date.now()
+//     };
+//     this.setState(state => ({
+//       items: state.items.concat(newItem),
+//       text: ''
+//     }));
+//   }
+// }
+
+// class TodoList extends React.Component {
+//   render() {
+//     return (
+//       <ul>
+//         {this.props.items.map(item => (
+//           <li key={item.id}>{item.text}</li>
+//         ))}
+//       </ul>
 //     );
 //   }
 // }
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>To Do</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="new-todo">
-            What needs to be done?
-          </label>
-          <input
-            id="new-todo"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
-        </form>
-      </div>
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.text.length === 0) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
-  }
-}
-
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-    );
-  }
-}
-
-ReactDOM.render(
-  <TodoApp />,
-  document.getElementById('todos-example')
-);
+// ReactDOM.render(
+//   <TodoApp />,
+//   document.getElementById('todos-example')
+// );
 
 
-export default withAuthenticator(TodoApp, true);
+export default withAuthenticator(App, true);
